142661de97293d18955b22a984beea4357cc02b4
Giang Vu <dhgiang.vu@gmail.com>
2024-09-26 19:22:45 +1000

    add sprinting and stamina

---
+++ Assets/Scripts/Character/StaminaManager.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * Logic: stop player from being able to sprint for a while if stamina is depleted to 0.
+*/
+public class StaminaManager : MonoBehaviour
+{
+    [SerializeField] private float staminaCap = 10.0f;
+    [SerializeField] private float staminaDeductionRate = 5.0f;
+    [SerializeField] private float staminaRecoveryRate = 2.0f;
+    [SerializeField] private float staminaDepletedTime = 5.0f;
+    private float currentStamina;
+    private bool staminaDepleted;
+
+    void Start()
+    {
+        currentStamina = staminaCap;
+        staminaDepleted = false;
+    }
+
+    public bool CanSprint()
+    {
+        return !staminaDepleted;
+    }
+
+    public void RecoverStamina()
+    {
+        if (currentStamina < staminaCap)
+        {
+            currentStamina += staminaRecoveryRate * Time.deltaTime;
+        }
+
+        if (staminaDepleted)
+        {
+            Debug.Log("Stamina has been depleted. Cannot sprint temporarily. Recovering Stamina: " + currentStamina.ToString());
+        }
+        else
+        {
+            Debug.Log("Recovering Stamina: " + currentStamina.ToString());
+        }
+    }
+
+    public void ConsumeStamina()
+    {
+        if (staminaDepleted)
+        {
+            return;
+        }
+
+        currentStamina -= staminaDeductionRate * Time.deltaTime;
+        
+        if (currentStamina <= 0)
+        {
+            staminaDepleted = true;
+            StartCoroutine(RecoverDepletedStamina());
+        }
+
+        Debug.Log("Consuming Stamina: " + currentStamina.ToString());
+    }
+
+    private IEnumerator RecoverDepletedStamina()
+    {
+        yield return new WaitForSeconds(staminaDepletedTime);
+        staminaDepleted = false;
+    }
+}
