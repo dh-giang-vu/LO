46b6886adbd4ce4a741b5a02ab9428683fb23fc0
screentime0 <apriyatama@student.unimelb.edu.au>
2024-10-13 12:59:39 +1100

    Particle system seperated

--- Assets/_Scripts/Camera/ScreenEffectManager.cs
+++ Assets/_Scripts/Camera/ScreenEffectManager.cs
@@ -1,71 +1,78 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Rendering;
 
 public class ScreenEffectManager : MonoBehaviour
 {
     [SerializeField] ScreenPostProcessing screenPostProcessing;
-    [SerializeField] ParticleSystem lightSourceParticles;
-    [SerializeField] ParticleSystem buildingParticles;
     [SerializeField] SanityManager sanityManager;
+
     bool inBuilding = false;
     bool inLight = false;
-    // Start is called before the first frame update
-    void Start()
-    {
-    }
 
-    // Update is called once per frame
     void Update()
     {
         WithinAreaEffect();
-        if (inBuilding) {
+
+        if (inBuilding)
+        {
             BuildingEffectOn();
-        } else {
+        }
+        else
+        {
             BuildingEffectOff();
         }
-        if (inLight) {
+
+        if (inLight)
+        {
             LightSourceEffectOn();
-        } else {
+        }
+        else
+        {
             LightSourceEffectOff();
         }
     }
 
-    public void WithinAreaEffect() {
+    public void WithinAreaEffect()
+    {
         inBuilding = false;
         inLight = false;
+
         List<ISanityProvider> sanityProviders = sanityManager.GetActiveSanityProviders();
-        foreach (ISanityProvider provider in sanityProviders) {
-            if (provider.getSanityEffect() == 0f) {
-                inLight = true;
-            } else {
-                inBuilding = true;
+        foreach (ISanityProvider provider in sanityProviders)
+        {
+            if (provider.getSanityEffect() == 0f)
+            {
+                inLight = true;  // Player is near a light source
+            }
+            else
+            {
+                inBuilding = true;  // Player is inside a building
             }
         }
     }
 
-    public void BuildingEffectOn() {
+    public void BuildingEffectOn()
+    {
         Debug.Log("TURNING ON BUILDING FILTER");
         screenPostProcessing.BuildingFilterOn();
-        buildingParticles.Play();
     }
 
-    public void BuildingEffectOff() {
+    public void BuildingEffectOff()
+    {
         Debug.Log("TURNING OFF BUILDING FILTER");
         screenPostProcessing.BuildingFilterOff();
-        buildingParticles.Stop();
-    }
-    
-    public void LightSourceEffectOn() {
-        Debug.Log("TURNING ON LIGHT FILTER");
-        screenPostProcessing.LightSourceFilterOn();
-        lightSourceParticles.Play();
     }
 
-    public void LightSourceEffectOff() {
+    public void LightSourceEffectOn()
+    {
+        Debug.Log("TURNING ON LIGHT FILTER");
+        screenPostProcessing.LightSourceFilterOn();
+    }
+
+    public void LightSourceEffectOff()
+    {
         Debug.Log("TURNING OFF LIGHT FILTER");
         screenPostProcessing.LightSourceFilterOff();
-        lightSourceParticles.Stop();
     }
 }
---
+++ Assets/_Scripts/ParticleSystem/ParticleEffectController.cs
@@ -0,0 +1,35 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingObjectToggle : MonoBehaviour
+{
+    [SerializeField] private SanityManager sanityManager; // Reference to the SanityManager
+    [SerializeField] private GameObject objectToToggle;   // The GameObject to enable/disable
+
+    private bool inBuilding = false;
+
+    void Update()
+    {
+        CheckBuildingStatus();
+
+        // Enable or disable the GameObject based on the inBuilding status and sanity level
+        float currentSanity = sanityManager.GetSanityAmount();
+        objectToToggle.SetActive(inBuilding && currentSanity < 1.0f); // Disable if sanity is full
+    }
+
+    private void CheckBuildingStatus()
+    {
+        inBuilding = false; // Reset the status
+
+        // Get the active sanity providers to check the building status
+        List<ISanityProvider> sanityProviders = sanityManager.GetActiveSanityProviders();
+        foreach (ISanityProvider provider in sanityProviders)
+        {
+            if (provider.getSanityEffect() != 0f)
+            {
+                inBuilding = true; // Player is inside a building
+                break; // No need to check further, we found that the player is in a building
+            }
+        }
+    }
+}
