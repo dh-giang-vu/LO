345fbc42e1f9c597de4ca47d4bd9ad8e5ba499bb
Giang Vu <dhgiang.vu@gmail.com>
2024-10-20 21:07:52 +1100

    fix stop sanity gain sound effect when sanity full

--- Assets/_Scripts/ParticleSystem/ParticleEffectController.cs
+++ Assets/_Scripts/ParticleSystem/ParticleEffectController.cs
@@ -5,10 +5,16 @@ public class BuildingObjectToggle : MonoBehaviour
 {
     [SerializeField] private SanityManager sanityManager; // Reference to the SanityManager
     [SerializeField] private GameObject objectToToggle;   // The GameObject to enable/disable
+    AudioSource audioSource;
     [SerializeField] private AudioClip gainSanityAudio;   // The GameObject to enable/disable
-    [SerializeField, Range(0,1)] private float audioVolume = 1.0f;
+    [SerializeField, Range(0, 1)] private float audioVolume = 1.0f;
 
     private bool inBuilding = false;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
 
     void Update()
     {
@@ -17,9 +23,22 @@ public class BuildingObjectToggle : MonoBehaviour
         // Enable or disable the GameObject based on the inBuilding status and sanity level
         float currentSanity = sanityManager.GetSanityAmount();
         objectToToggle.SetActive(inBuilding && currentSanity < 1.0f); // Disable if sanity is full
+        
         if (inBuilding && currentSanity < 1.0f)
         {
-            AudioSource.PlayClipAtPoint(gainSanityAudio, transform.position, audioVolume);
+            // If audio is not already playing, play the sanity gain sound
+            if (!audioSource.isPlaying)
+            {
+                audioSource.PlayOneShot(gainSanityAudio, audioVolume);
+            }
+        }
+        else
+        {
+            // Stop the audio if the player is not in the building or sanity is full
+            if (audioSource.isPlaying)
+            {
+                audioSource.Stop();
+            }
         }
     }
 
